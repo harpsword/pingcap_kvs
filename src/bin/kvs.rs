521248a76d90d828476c39a3337a86b040627106
harpsword <harpswordyyl@gmail.com>
2022-06-05 10:31:47 +0800

    feat: finish project 1

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,49 +1,47 @@
-use clap::{App, AppSettings, Arg, SubCommand};
-use std::process::exit;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(name = "kvs")]
+#[clap(author, version, about = "A key-value store", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    comamnd: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[clap(arg_required_else_help = true)]
+    Get { key: String },
+
+    #[clap(arg_required_else_help = true)]
+    Set { key: String, value: String },
+
+    #[clap(arg_required_else_help = true, name = "rm")]
+    Remove { key: String },
+}
 
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .setting(AppSettings::DisableHelpSubcommand)
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("Set the value of a string key to a string")
-                .arg(Arg::with_name("KEY").help("A string key").required(true))
-                .arg(
-                    Arg::with_name("VALUE")
-                        .help("The string value of the key")
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("Get the string value of a given string key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("Remove a given key")
-                .arg(Arg::with_name("KEY").help("A string key").required(true)),
-        )
-        .get_matches();
+    let args = Cli::parse();
 
-    match matches.subcommand() {
-        ("set", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
+    let store = kvs::KvStore::new();
+
+    match args.comamnd {
+        Commands::Get { key } => {
+            println!("get key: {key}");
+            eprint!("unimplemented");
+            std::process::exit(1);
         }
-        ("get", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
+        Commands::Set { key, value } => {
+            println!("set key {key}, value {value}");
+            eprint!("unimplemented");
+            std::process::exit(1);
         }
-        ("rm", Some(_matches)) => {
-            eprintln!("unimplemented");
-            exit(1);
+        Commands::Remove { key } => {
+            println!("remove key {key}");
+            eprint!("unimplemented");
+            std::process::exit(1);
         }
-        _ => unreachable!(),
     }
+
+    println!("Hello, World!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
-#![deny(missing_docs)]
-//! A simple key/value store.
+use std::{collections::HashMap, hash::Hash};
 
-pub use kv::KvStore;
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
 
-mod kv;
+impl KvStore {
+    pub fn new() -> Self {
+        KvStore {
+            store: HashMap::new(),
+        }
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        _ = self.store.insert(key, value)
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        self.store.get(&key).map(|v| v.clone())
+    }
+
+    pub fn remove(&mut self, key: String) {
+        _ = self.store.remove(&key)
+    }
+}
